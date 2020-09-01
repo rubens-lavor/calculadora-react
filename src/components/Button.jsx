9925a37e20a7429a7f4abbd8578f13f613f00194
rubens-lavor <haryson07@gmail.com>
2020-09-01 18:08:02 -0300

    botões

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Button.css"
+
+export default (props) => {
+    return(
+        <button className="button">{props.label}</button>
+        
+
+    )
+}
